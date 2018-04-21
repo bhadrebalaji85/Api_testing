842233d6d35291b5ed123c59356dd309c0ffc99a
balaji.bhadre85 <balaji.bhadre85@gmail.com>
2018-04-21 18:48:09 +0530

    Api testing

---
+++ com.google.api/src/test/java/ApiTesting/com/google/api/Weather_byCity.java
@@ -0,0 +1,71 @@
+package ApiTesting.com.google.api;
+
+import javax.mail.internet.ContentType;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import com.jayway.restassured.response.Response;
+
+import static com.jayway.restassured.RestAssured.*;
+
+public class Weather_byCity {
+  @Test
+  public void Test_01() {
+	  
+	  Response res= when().
+	  get("http://api.openweathermap.org/data/2.5/weather?" +
+	  		"q=London&appid=ab7c75acbd12c739ec967844e747e448");
+	  
+	  System.out.println(res.getStatusCode());
+	  
+	  Assert.assertEquals(res.getStatusCode(), 200);
+	  
+  }
+  
+  @Test
+  public void Test_02() {
+	  
+	  Response res= when().
+	  get("http://api.openweathermap.org/data/2.5/weather?" +
+	  		"q=London&appid=ab7c75acbd12c739ec967844e747e448");
+	  
+	  System.out.println(res.getStatusCode());
+	  
+	  Assert.assertEquals(res.getStatusCode(), 200);
+	  
+	  System.out.println(res.asString());
+  }
+  
+  @Test
+  public void Test_03()
+  {
+	  given().
+	  param("q","London").
+	  param("appid","ab7c75acbd12c739ec967844e747e448").
+	  when().
+	  get("http://api.openweathermap.org/data/2.5/weather").
+	  then().
+	  assertThat().statusCode(200);
+	  
+  }
+  
+  @Test
+  public void Test_03_jsonpath()
+  {
+	  String jsonpath=given().
+	  param("q","London").
+	  param("appid","ab7c75acbd12c739ec967844e747e448").
+	  when().
+	  get("http://api.openweathermap.org/data/2.5/weather").
+	  then().
+      contentType(com.jayway.restassured.http.ContentType.JSON).
+	  extract().
+	  
+	  path("sys.country");
+	  //assertThat().statusCode(200);
+	  
+	  System.out.println("Current Humidity"+jsonpath);
+	  
+  }
+}
